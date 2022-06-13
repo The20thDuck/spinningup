662547363efd66c57679c8c7d9cafc879964aba4
Warren Sunada-Wong <warren.sunada.wong@gmail.com>
2022-06-13 17:02:45 -0400

    Change requirements for M1

--- spinup/utils/run_entrypoint.py
+++ spinup/utils/run_entrypoint.py
@@ -7,5 +7,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('encoded_thunk')
     args = parser.parse_args()
+    # import ipdb; ipdb.set_trace()
+    # print(zlib.decompress(base64.b64decode(args.encoded_thunk)))
     thunk = pickle.loads(zlib.decompress(base64.b64decode(args.encoded_thunk)))
+    # print(thunk)
     thunk()
